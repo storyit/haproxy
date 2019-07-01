0e7079d843e38c862d212e39a137a510713b2da8
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-07-01 13:53:41 -0500

    also forgot to bind mount actual certs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name frontend -p 443:443 -p 80:80 -v $(pwd):/usr/local/etc/haproxy:ro load-balancer
+docker run -d --name frontend -p 443:443 -p 80:80 -v $(pwd):/usr/local/etc/haproxy:ro -v /etc/ssl/haproxy/:/etc/ssl/haproxy/ load-balancer
